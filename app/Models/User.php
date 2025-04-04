e640b155b60b58a463cf07238475f4e0577185ce
mustafamsm <75886186+mustafamsm@users.noreply.github.com>
2025-04-04 21:08:35 +0300

    setup auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'photo',
+        'phone',
+        'address',
+        'role',
+        'status',
     ];
 
     /**
